eae1bea67f3c344da41f4585812e8910e5766016
alejosebastianali <74724530+alejosebastianali@users.noreply.github.com>
2021-06-25 17:23:13 -0300

    Add files via upload

--- script_01.js
+++ script_01.js
@@ -1,6 +1,6 @@
 window.onload=function(){
   $('.slider').slick({
-  autoplay:true,
+  autoplay:false,
   autoplaySpeed:1500,
   arrows:true,
   prevArrow:'<button type="button" class="slick-prev"></button>',
